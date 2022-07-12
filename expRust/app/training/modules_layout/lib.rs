2cafd46e7ec66b5cab8ac4875d3a7143b0b92137
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2022-07-12 18:11:06 +0800

    expRust/app/training: added modules_layout training project
    Since project layout is very demanding on my side to merge Go and
    Rust library codes into one, I have to explore and tried it out
    here. Hence, let's do this.
    
    This patch adds modules_layouts training project into
    expRust/app/training/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ expRust/app/training/modules_layout/lib.rs
@@ -0,0 +1,46 @@
+// Copyright 2022 "Holloway" Chew, Kean Ho <hollowaykeanho@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+pub mod back_of_house;
+pub mod front_of_house;
+
+pub fn eat_at_restaurant() {
+	// Absolute Path
+	crate::front_of_house::hosting::add_to_waitlist();
+
+	// Relative Path
+	front_of_house::hosting::add_to_waitlist();
+
+	let mut meal = back_of_house::Breakfast::Meal::summer("Rye");
+	meal.toast = String::from("Wheat");
+	println!("I'd like {} toast please", meal.toast);
+
+	let order1 = back_of_house::Appetizer::Meal::Soup;
+	match order1 {
+		| back_of_house::Appetizer::Meal::Soup => {
+			println!("I'd like soup as appetizer please")
+		},
+		| back_of_house::Appetizer::Meal::Salad => {
+			println!("I'd like salad as appetizer please")
+		},
+	}
+}
+
+fn delivery_order() {}
